8c872906562af8ff5466c0f28ad5749e866eb588
Meyer Nigrini <Meyer@nebula.co.za>
2025-01-17 11:20:45 +0200

    Add profile picture and change styling

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,16 @@
-import { Button, Container, Title, Text } from "@mantine/core";
-
+import { Button, Container, Title, Text, Image } from "@mantine/core";
+import myImage from "./assets/ProfilePic.jpg";
 
 function Home(){
     return (
         <Container>
-            <section style={{textAlign: "center", padding: "50px 0"}}>
-                <Title> Welcome </Title>
-                <Text size="lg" mt="sm"> My name is Meyer Nigrini</Text>
-                <Button>Details</Button>
+            <section style={{display: "flex", alignItems: "center", justifyContent: "space-between" , padding: "50px 50px"}}>
+                <div style = {{textAlign: "left"}}>
+                    <Title> Welcome </Title>
+                    <Text size="lg" mt="sm"> My name is Meyer Nigrini</Text>
+                    <Button>Details</Button>
+                </div>
+                <Image src={myImage} width={200} height={200} style={{borderRadius: "50%", objectFit: "cover"}}/>
             </section>
         </Container>
     )
